0172c0273b54bd287854b88d3a808c5eba8ad27e
Yamile-Quezada <mquezada1622@chih.conalep.edu.mx>
2025-05-28 08:19:43 -0600

    hola

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+    $server ="localhost";
+    $user="root";
+    $pass="";
+    $db="alumnos";
+    $conexion= new mysqli($server,$user,$pass,$db);
+        if($conexion->connect_errno){
+            die("Conexion Fallida".
+            $conexion->$conexion->connect_errno);
+        } else{
+            echo "Conectado"; //hola jajjka
+        }
+?>
---
+++ insert.php
@@ -0,0 +1,17 @@
+<?php
+require_once "conexion.php";
+$db="alumnos";
+$nombre=$_POST['nombre'];
+$correo=$_POST['correo'];
+$pass=$_POST['pass'];
+
+echo<br>Datos recibidos:<br><br>;
+echo=$nombre, $correo,$pass<br>;
+$sql="INSERT INTO informacion(id,nombre,correo,pass)VALUES(null,'$nombre','$correo','$pass')";
+if(mysqli($conexion,$sql)){
+    echo"<br>Nuevo registro";
+}else{
+    echo "Error: ".$sql."<br>" .mysqli_error($conexion);
+}
+mysqli_close($conexion); //hola jajjka
+?> 
