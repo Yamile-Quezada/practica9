1e652b9fdf3d513d465236d61031bc2fa73ecc8a
Yamile-Quezada <mquezada1622@chih.conalep.edu.mx>
2025-05-29 08:12:37 -0600

    hola2

--- insert.php
+++ insert.php
@@ -1,14 +1,14 @@
 <?php
 require_once "conexion.php";
 $db="alumnos";
-$nombre=$_POST['nombre'];
-$correo=$_POST['correo'];
-$pass=$_POST['pass'];
+$nombre= $_POST['nombre'];
+$correo= $_POST['correo'];
+$pass= $_POST['pass'];
 
-echo<br>Datos recibidos:<br><br>;
-echo=$nombre, $correo,$pass<br>;
-$sql="INSERT INTO informacion(id,nombre,correo,pass)VALUES(null,'$nombre','$correo','$pass')";
-if(mysqli($conexion,$sql)){
+echo "<br>". "Datos recibidos:". "<br><br>";
+echo "$nombre, $correo, $pass<br>";
+$sql= "INSERT INTO informacion(id,nombre,correo,pass)VALUES(null,'$nombre','$correo','$pass')";
+if(mysqli_query($conexion,$sql)){
     echo"<br>Nuevo registro";
 }else{
     echo "Error: ".$sql."<br>" .mysqli_error($conexion);
